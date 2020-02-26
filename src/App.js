19bac07dc7689c71d9251a4237340b90fc0d7e28
lsb <leebutterman@gmail.com>
2020-02-25 17:43:55 -0800

    Add sample latents and use trixie-brian/lsb4-lsb5 for face morphing

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ImageUploader from 'react-images-upload';
 import './App.css';
+import {decode as u64toArrayBuffer, encode as arrayBufferToU64} from 'base64url-arraybuffer';
 
 const ENDPOINT = "http://localhost:5000";
 const ENDPOINT_ALIGNFACES = (u64rawfaces) => `${ENDPOINT}/alignfaces?u64rawfaces=${u64rawfaces}`;
@@ -12,10 +13,43 @@ const serverU64ToDataURLB64 = (u64) => `data:image/webp;base64,${u64.replace(/[-
 
 const zeroFaceU64 = Array.from({length: 18 * 512 * 4 * 4 / 3}, () => "A").join("");
 
+const saxpy = (a,x,y) => console.log({a,x,y}) || Float32Array.from({length: x.length}, (v,k) => a * x[k] + y[k]);
+
+const u64ToFloats = (u64) => new Float32Array(u64toArrayBuffer(u64));
+const floatsToU64 = (f32) => arrayBufferToU64(f32.buffer);
+
+const extendLatents = (faceToMorph, sliderIntensity, dropdownMorphChooser, availableFaces, distanceInMorph) => floatsToU64(saxpy(distanceInMorph * (sliderIntensity / 10), getDirection(dropdownMorphChooser, availableFaces), u64ToFloats(faceToMorph)));
+
+const getDirection = (dropdownMorphChooser, availableFaces) => {
+  const [src, dst] = dropdownMorphChooser.split(' ').map(f => availableFaces.get(f));
+  return saxpy(-1, u64ToFloats(src), u64ToFloats(dst));
+}
+
 class App extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {imageDataURL: null, u64AlignedFaces: null, u64Latents: null};
+    this.state = {imageDataURL: null, u64AlignedFaces: null, u64Latents: null, facemathslider: 3, facemathdropdown: "brian trixie2"};
+    const faceURLs = new Map([
+      ["brian", "./brian-firkus3_01.latents"],
+      ["trixie1", "./trixie-mattel1_01.latents"],
+      ["trixie2", "./trixie-mattel-2_01.latents"],
+      ["lsb1", "./lsb1_01.latents"],
+      ["lsb2", "./lsb2_01.latents"],
+      ["lsb3", "./lsb3_01.latents"],
+      ["lsb4", "./lsb4_01.latents"],
+      ["lsb5", "./lsb5_01.latents"],
+    ]);
+    this.loadFaces(faceURLs);
+  }
+  async loadFaces(faceURLs) {
+    const err = (r) => this.setState({error: `loadFaces error ${r.status}`});
+    const faces = new Map();
+    for(let [faceKey, facePath] of faceURLs) {
+      for await (const line of fetchNDUTF8Lines(facePath, err)) {
+        faces.set(faceKey, line);
+      }
+    }
+    this.setState({faces});
   }
   async optimize() {
     if(this.state.optimizeLatch === true) {
@@ -29,8 +63,12 @@ class App extends React.Component {
     if(u64Latents === null || u64AlignedFaces === null || u64AlignedFaces.length === 0) return;
     const url = ENDPOINT_OPTIMIZE(u64AlignedFaces[0], u64Latents);
     const err = (r) => this.setState({error: `OPTIMIZE error ${r.status}`});
-    for await (const line of fetchNDUTF8Lines(url, err)) {
-      this.setState({u64Latents: line});
+    try {
+      for await (const line of fetchNDUTF8Lines(url, err)) {
+        this.setState({u64Latents: line});
+      }
+    } catch (e) {
+      this.setState({error: `OPTIMIZE error run ${e}`});
     }
     this.state.optimizeLatch = false;
     this.setState({lastOptimize: Date.now()})
@@ -55,12 +93,21 @@ class App extends React.Component {
   render() {
     return (
       <div className="App">
-        <h1>Ambush selfie makeover</h1>
+        <h1>Face-math selfie makeover</h1>
         <div className="error">
           {this.state.error}
         </div>
         <div className="picker">
           <ImageUploader withIcon={true} buttonText="Let me take a selfie" onChange={(f,u) => this.onDrop(f,u)} singleImage={true} />
+        </div>
+        <div className="facemath">
+          <input type="range" min={0} max={11} value={this.state.facemathslider} onChange={e => this.setState({facemathslider: e.target.value * 1})} /> ± {this.state.facemathslider * 10}%<br/>
+          <select value={this.state.facemathdropdown} onChange={e => this.setState({facemathdropdown: e.target.value})}>
+            <option value="brian trixie2">± Trixie Mattel</option>
+            <option value="lsb4 lsb5">± Glasses</option>
+          </select>
+          <hr/>
+          {this.state.u64Latents === null ? "" : [-1,-0.5,-0.25,0,0.25,0.5,1].map(d => (<img src={ENDPOINT_RENDER9000(extendLatents(this.state.u64Latents, this.state.facemathslider, this.state.facemathdropdown, this.state.faces, d))} />))}
         </div>
         <div>
           &nbsp; {this.state.optimizeLatch ? "⚡" : ""} {this.state.optimizeAgain ? "𝄎" : ""}
